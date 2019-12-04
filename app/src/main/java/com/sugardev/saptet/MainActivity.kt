960e323f46e05d2d778e15957c9d2134a52c8423
Sugar <thuyetduongg@gmail.com>
2019-12-04 11:36:01 +0700

    fix ui

--- app/src/main/java/com/sugardev/saptet/MainActivity.kt
+++ app/src/main/java/com/sugardev/saptet/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.sugardev.saptet
 
 
+import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.os.Handler
@@ -12,20 +13,36 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.text.SimpleDateFormat
 import java.util.*
 
+
 class MainActivity : AppCompatActivity() {
 
     private val handle = Handler()
     private val tetHolidays = "25/01/2020"
     private val pattern = "dd/MM/yyyy"
     private lateinit var cd: CountDownTimer
+    private lateinit var mediaPlayer: MediaPlayer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         this.turnOnFullScreen()
+        initPlayer()
         starAnim()
         pauseAnim()
         initView()
+        initEventClick()
+
+    }
+
+    private fun initPlayer() {
+        mediaPlayer = MediaPlayer.create(applicationContext, R.raw.lk_xuan)
+    }
+
+    private fun initEventClick() {
+        ivMusic.setOnClickListener {
+            if (mediaPlayer.isPlaying) mediaPlayer.pause()
+            else playSong()
+        }
     }
 
 
@@ -39,7 +56,7 @@ class MainActivity : AppCompatActivity() {
 
             override fun onTick(p0: Long) {
                 tvDate.text = String.format("%02d", p0 / (24 * 60 * 60 * 1000))
-                tvHour.text = String.format("%02d",  p0 / (60 * 60 * 1000) % 24)
+                tvHour.text = String.format("%02d", p0 / (60 * 60 * 1000) % 24)
                 tvMinute.text = String.format("%02d", p0 / (60 * 1000) % 60)
                 tvSecond.text = String.format("%02d", p0 / 1000 % 60)
             }
@@ -58,11 +75,22 @@ class MainActivity : AppCompatActivity() {
         }, 2000)
     }
 
+    private fun playSong() {
+        mediaPlayer.start()
+    }
+
     private fun starAnim() {
         animLottie.playAnimation()
     }
 
+    override fun onPause() {
+        mediaPlayer.pause()
+        super.onPause()
+    }
+
     override fun onDestroy() {
+        mediaPlayer.stop()
+        mediaPlayer.release()
         handle.removeCallbacksAndMessages(null)
         super.onDestroy()
     }
